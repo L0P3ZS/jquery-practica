64db1a436f9ece5d9996c4db8e951652172d52c8
Sebastian <auxiliartics1agssalud@gmail.com>
2024-06-17 17:00:51 -0500

    Nuevas

---
+++ manipulacionDelDom/js/jscritp2.js
@@ -0,0 +1,7 @@
+$("#eliminar").click(function () { 
+
+//con el remove se puede eliminae elementos de la pagina
+   $('p').remove();
+   
+});
+
---
+++ manipulacionDelDom/js/jscritp3.js
@@ -0,0 +1,10 @@
+$("#modificar").click(function () { 
+
+//html sirve para modificar
+   $('#parrafo').html('nuevo texto');
+// El addClass para agregar un clase 
+   $('#parrafo').addClass('parr');
+//El attr sirve para agregar atrivutos 
+   $('#parrafo').attr('title', 'texto agregado');
+});
+
